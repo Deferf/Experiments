1b1e369feef3562a9a0fa2712bb15c6e4a5efca4
Deferf <japortilloq@gmail.com>
2020-09-24 21:19:38 -0700

    Update models.py

--- models.py
+++ models.py
@@ -8,9 +8,9 @@ def center_branch_simple_model(dim_left, dim_right, dim_dense = 2048, activation
   center_left = Lambda(lambda x: x[:,:dim_left])(center_input)
   center_right = Lambda(lambda x: x[:,-dim_right:])(center_input)
 
-  left_branch = Dense(dim_dense, input_dim=4096, name='left_branch', activation=activation, bias = bias)(center_left)
+  left_branch = Dense(dim_dense, input_dim=4096, name='left_branch', activation=activation, use_bias = bias)(center_left)
 
-  right_branch = Dense(dim_dense, input_dim=768, name='right_branch', activation=activation, bias = bias)(center_right)
+  right_branch = Dense(dim_dense, input_dim=768, name='right_branch', activation=activation, use_bias = bias)(center_right)
 
   salida = Concatenate(axis=1, name = 'salida')([left_branch, right_branch])
 
