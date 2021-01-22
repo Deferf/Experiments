d46686b51fcfe87d2add03f97090c8b309e3246b
Deferf <japortilloq@gmail.com>
2021-01-22 14:03:00 -0700

    New functions
    Added an enhanced version of recal lmetrics called rank at k

--- metrics.py
+++ metrics.py
@@ -26,3 +26,27 @@ def recall_metrics(alpha,beta, k = [1,5,10], s = 1):
     start = end
   
   return {"R@" + str(a): [results[a]/length] for a in results}
+
+
+def rank_at_k(alpha,beta, k = [1,5,10], s = 1):
+  # To create the rectangular rank matrix we need some milestones to divide
+  length = alpha.shape[0] #height
+  limits = [a+1 for a in range(0,length,int(length/s))] # creates upper limits
+  start = 0
+  results = {a: 0 for a in k}
+  rank_list = []
+  for l in limits:
+    end = l
+    rm = rank_matrix(alpha[start:end],beta) # a rectangle size limit size x width, with rows argsorted
+
+    for i,row in enumerate(rm):
+      rank_list.append(int(row[start + i]) + 1) # identify the sorting position and add 1 to convert to rank
+      for clip in k:
+        if start + i in row[:clip]:
+          results[clip] = results[clip] + 1
+
+    start = end
+  
+  metrics = {"R@" + str(a): [results[a]/length] for a in results}
+  metrics["MedRank"] = statistics.median(rank_list)
+  return metrics
